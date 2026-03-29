e9e690692fafd66b35c6a8580d54b57273bdbc81
ehabassem3.lab <ehabassem3@gmail.com>
2026-03-29 14:08:06 +0200

    Initial Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    kotlin("plugin.serialization") version "1.9.0"
 }
 
 android {
@@ -42,6 +43,9 @@ android {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    val nav_version = "2.9.7"
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -50,6 +54,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/compose_first/MainActivity.kt
+++ app/src/main/java/com/example/compose_first/MainActivity.kt
@@ -1,135 +1,102 @@
 package com.example.compose_first
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Divider
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.VerticalDivider
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.VerticalAlignmentLine
-import androidx.compose.ui.layout.layout
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.compose_first.routes.HomeRoute
+import com.example.compose_first.routes.SplashRoute
+import com.example.compose_first.screens.HomeScreen
+import com.example.compose_first.screens.SplashScreen
 import com.example.compose_first.ui.theme.Compose_FirstTheme
+
 
 class MainActivity : ComponentActivity() {
 
 
 
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            first()
 
-        }
+          setContent{
+
+              Compose_FirstTheme() {
+                  App()
+              }
+
+          }
+
     }
 }
-
-
 @Composable
-fun first(){
+fun App(
 
-    var names  by remember { mutableStateOf(listOf<String>()) }
-    var name by remember { mutableStateOf("") }
-    Column(
-
-        modifier = Modifier.fillMaxSize().background(Color.White).padding(30.dp)
-       , horizontalAlignment = Alignment.CenterHorizontally,
-
-
-        ) {
-           Row() {
-               OutlinedTextField(value = name,onValueChange  = { text ->
-                   name = text
-
-               },modifier = Modifier.weight(1f) )
-               Button(
-
-                   onClick ={
-
-                           names = names +name
-                           name = ""
-
-
-                            },
-
-               )
-               {
-
-                   Text("Add Name ")
-
-
-               }
-
-           }
-        Spacer(modifier = Modifier.width(20.dp))
-        LazyColumn(modifier = Modifier.fillMaxSize()) {
-            items(names){ currenitems->
-
-                Text(
-                    text = currenitems,
-                    fontSize = 200.sp,
-                    color = Color.Black
-                )
-                HorizontalDivider()
-                VerticalDivider()
-
-
+){
+    var navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = HomeRoute
+    ) {
+        composable<HomeRoute> {
+            HomeScreen(navController)
         }
-
-
-
+        composable<SplashRoute> {
+            SplashScreen(navController)
         }
-
-
-
-
-
-
-
-
-
 
     }
+
 }
+
+
 
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Compose_FirstTheme {
-        first()
 
-    }
 }
